9143c36bcd29ac51cfd5b4f4bb872467060a09aa
Niklas <niklaslinz@hotmail.com>
2017-11-05 19:48:00 +0100

    Update copyright

--- src/main/java/skills/VIKITemplate.java
+++ src/main/java/skills/VIKITemplate.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2017. Niklas Linz - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LGPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the LGPLv3 license with
+ * this file. If not, please write to: niklas.linz@enigmar.de
+ *
+ */
+
 package skills;
 
 import de.linzn.leegianOS.LeegianOSApp;
